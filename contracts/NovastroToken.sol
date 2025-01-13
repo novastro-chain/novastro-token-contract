dbfb8c6acd3659a42454385a3e2c7df3ec3f5354
Krupa Sagar <sagar.krupahb2000@gmail.com>
2025-01-13 18:14:48 +0530

    Ownable is redundant

--- contracts/NovastroToken.sol
+++ contracts/NovastroToken.sol
@@ -2,12 +2,11 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NovastroToken is ERC20, Ownable {
+contract NovastroToken is ERC20 {
     uint256 public constant TOTAL_SUPPLY = 1_000_000_000 * 10**18; // 1 billion tokens
 
-    constructor() ERC20("NOVASTRO", "NOVAS") Ownable(msg.sender) {
+    constructor() ERC20("NOVASTRO", "NOVAS") {
         _mint(msg.sender, TOTAL_SUPPLY);
     }
 
