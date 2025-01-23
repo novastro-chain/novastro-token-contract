7f02bcdc1c8d77963f4ef8fbd510a8ee6ad9c060
shreedhar <shreedhar888@gmail.com>
2025-01-23 19:51:44 +0530

    fix code review

--- contracts/NovastroToken.sol
+++ contracts/NovastroToken.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract NovastroToken is ERC20, AccessControl {    
+contract NovastroToken is ERC20, AccessControl {
+    uint256 private constant MINIMUM_TRANSFER = 1000; // Minimum transfer amount
     uint256 public constant TOTAL_SUPPLY = 1_000_000_000 * 10**18; // 1 billion tokens
 
     constructor() ERC20("NOVASTRO", "NOVAS") {
@@ -12,11 +13,8 @@ contract NovastroToken is ERC20, AccessControl {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function transfer(address to, uint256 amount) public virtual override returns (bool) {
-        return super.transfer(to, amount);
-    }
-
-    function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
-        return super.transferFrom(from, to, amount);
+    function _update(address from, address to, uint256 value) internal virtual override {
+        require(value >= MINIMUM_TRANSFER || from == address(0) || to == address(0), "Transfer amount too low");
+        super._update(from, to, value);
     }
 }
